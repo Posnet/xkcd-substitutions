06fad01a94e7f5e7121508f40aac073eafdfffa2
Alec Posney <alec.posney@gmail.com>
2014-09-12 14:32:09 +1000

    customisation of config page

--- substitutions/js/options.js
+++ substitutions/js/options.js
@@ -1,31 +1,57 @@
-function save_options() {
-  var color = document.getElementById('color').value;
-  var likesColor = document.getElementById('like').checked;
-  chrome.storage.sync.set({
-    favoriteColor: color,
-    likesColor: likesColor
-  }, function() {
-    // Update status to let user know options were saved.
-    var status = document.getElementById('status');
-    status.textContent = 'Options saved.';
-    setTimeout(function() {
-      status.textContent = '';
-    }, 750);
-  });
-}
+// function save_options() {
+//   var color = document.getElementById('color').value;
+//   var likesColor = document.getElementById('like').checked;
+//   chrome.storage.sync.set({
+//     favoriteColor: color,
+//     likesColor: likesColor
+//   }, function() {
+//     // Update status to let user know options were saved.
+//     var status = document.getElementById('status');
+//     status.textContent = 'Options saved.';
+//     setTimeout(function() {
+//       status.textContent = '';
+//     }, 750);
+//   });
+// }
 
-// Restores select box and checkbox state using the preferences
-// stored in chrome.storage.
-function restore_options() {
-  // Use default value color = 'red' and likesColor = true.
-  chrome.storage.sync.get({
-    favoriteColor: 'red',
-    likesColor: true
-  }, function(items) {
-    document.getElementById('color').value = items.favoriteColor;
-    document.getElementById('like').checked = items.likesColor;
-  });
-}
-document.addEventListener('DOMContentLoaded', restore_options);
-document.getElementById('save').addEventListener('click',
-  save_options);
+// // Restores select box and checkbox state using the preferences
+// // stored in chrome.storage.
+// function restore_options() {
+//   // Use default value color = 'red' and likesColor = true.
+//   chrome.storage.sync.get({
+//     favoriteColor: 'red',
+//     likesColor: true
+//   }, function(items) {
+//     document.getElementById('color').value = items.favoriteColor;
+//     document.getElementById('like').checked = items.likesColor;
+//   });
+// }
+// document.addEventListener('DOMContentLoaded', restore_options);
+// document.getElementById('save').addEventListener('click',
+//   save_options);
+
+$(document).ready(function(){
+    var next = 1;
+    $(".add-more").click(function(e){
+        e.preventDefault();
+        var addto = "#field" + next;
+        var addRemove = "#field" + (next);
+        next = next + 1;
+        var newIn = '<input autocomplete="off" class="input form-control" id="field' + next + '" name="field' + next + '" type="text">';
+        var newInput = $(newIn);
+        var removeBtn = '<button id="remove' + (next - 1) + '" class="btn btn-danger remove-me" ><i class="fa fa-remove"></i></button></div><div id="field">';
+        var removeButton = $(removeBtn);
+        $(addto).after(newInput);
+        $(addRemove).after(removeButton);
+        $("#field" + next).attr('data-source',$(addto).attr('data-source'));
+        $("#count").val(next);  
+        
+            $('.remove-me').click(function(e){
+                e.preventDefault();
+                var fieldNum = this.id.charAt(this.id.length-1);
+                var fieldID = "#field" + fieldNum;
+                $(this).remove();
+                $(fieldID).remove();
+            });
+    }); 
+});
