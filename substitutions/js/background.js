68c48267953d1cf02dd5b51679de58153eaaf700
PorygonZRocks <PorygonZRocks@users.noreply.github.com>
2017-06-14 09:31:12 -0500

    "my cat" substitutions
    From https://github.com/Posnet/xkcd-substitutions/pull/52/

--- substitutions/js/background.js
+++ substitutions/js/background.js
@@ -104,6 +104,13 @@ var default_replacements = [
   //https://xkcd.com/1004/
   ['batman', 'a man dressed like a bat'],
   
+  //https://xkcd.com/1689/
+  ['my cat', 'my friend Catherine'],
+  ['his cat', 'his friend Catherine'],
+  ['her cat', 'her friend Catherine'],
+  ['their cat', 'their friend Catherine'],
+  ['your cat', 'your friend Catherine'],
+  
   //https://xkcd.com/1704/
   ['no man', 'Gnome Ann']
 ];
